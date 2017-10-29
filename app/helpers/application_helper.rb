61e13e92381b7be553d3395c0987a5ad03ca017b
Daniela Grossmann <daniela.grossmann@gmail.com>
2017-10-28 17:28:19 -0700

    adding subdirs to the side menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
   def navigation
     [
-        {name: 'Home', path: root_path},
-        {name: 'Manage', path: manage_path, if_clause: current_user && current_user.is_admin},
+        {name: 'Home', path: root_path, icon: 'home', sub_nav: [
+            {name: 'Location & Schedule', path: map_path, icon: 'question_answer'},
+            {name: 'Resources & FAQ', path: faq_path, icon: 'question_answer'},
+        ]},
+        {name: 'Manage', path: manage_path, icon: 'perm_data_setting', if_clause: current_user && current_user.is_admin},
     ]
   end
 end
