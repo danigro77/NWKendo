c0b742717ed82a0e955c8c84c339492d5a8d9603
Daniela Grossmann <daniela.grossmann@gmail.com>
2017-11-12 17:36:41 -0800

    trying to fix heroku 4

--- app/assets/javascripts/faq-resources.js
+++ app/assets/javascripts/faq-resources.js
@@ -5,10 +5,10 @@ document.addEventListener("turbolinks:load", function() {
         if (currentOpen.length > 0) {
             currentOpen.removeClass('col-12 open-card').addClass('col-5');
             if (!currentOpen.hasClass(identifier)) {
-                $(`.${identifier}`).removeClass('col-5').addClass('col-12 open-card');
+                $('.' + identifier).removeClass('col-5').addClass('col-12 open-card');
             }
         } else {
-            $(`.${identifier}`).removeClass('col-5').addClass('col-12 open-card');
+            $('.' + identifier).removeClass('col-5').addClass('col-12 open-card');
         }
         $('html, body').animate({
             scrollTop: $(`#${identifier}`).offset().top -100
--- app/assets/javascripts/manage.js
+++ app/assets/javascripts/manage.js
@@ -3,15 +3,15 @@ document.addEventListener("turbolinks:load", function() {
         var classes = this.className.split(/\s+/);
         var areaName = classes[2];
         if (classes.includes('arrow-up')) {
-            $(`#${areaName} .accordion-actions i.arrow-up`).hide();
-            $(`#${areaName} .accordion-actions i.arrow-down`).show();
-            $(`#${areaName} .result-block`).hide();
-            $(`#${areaName} .sub-header`).addClass('collapsed');
+            $('#' + areaName + ' .accordion-actions i.arrow-up').hide();
+            $('#' + areaName + ' .accordion-actions i.arrow-down').show();
+            $('#' + areaName + ' .result-block').hide();
+            $('#' + areaName + ' .sub-header').addClass('collapsed');
         } else {
-            $(`#${areaName} .accordion-actions i.arrow-up`).show();
-            $(`#${areaName} .accordion-actions i.arrow-down`).hide();
-            $(`#${areaName} .result-block`).show();
-            $(`#${areaName} .sub-header`).removeClass('collapsed');
+            $('#' + areaName + ' .accordion-actions i.arrow-up').show();
+            $('#' + areaName + ' .accordion-actions i.arrow-down').hide();
+            $('#' + areaName + ' .result-block').show();
+            $('#' + areaName + ' .sub-header').removeClass('collapsed');
         }
     });
 });
